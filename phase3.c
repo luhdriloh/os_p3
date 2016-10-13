98200dffc5a02061df92aa35871c6c4e94be002e
luhdriloh <smoore5@email.arizona.edu>
2016-10-13 12:14:45 -0700

    add new code

--- phase3.c
+++ phase3.c
@@ -6,8 +6,10 @@
 #include <sems.h>
 
 
-int
-start2(char *arg)
+proc ProcTable[MAXPROC];
+
+
+int start2(char *arg)
 {
     int pid;
     int status;
@@ -48,13 +50,234 @@ start2(char *arg)
      * values back into the sysargs pointer, switch to user-mode, and 
      * return to the user code that called Spawn.
      */
+
+    /* give each process tableslot its own personal mailbox */
+    for (int i = 0; i < MAXPROC; i++) {
+        ProcTable[i].mailBox = MboxCreate(0,0);
+    }
+
+
     pid = spawnReal("start3", start3, NULL, USLOSS_MIN_STACK, 3);
 
     /* Call the waitReal version of your wait code here.
      * You call waitReal (rather than Wait) because start2 is running
-     * in kernel (not user) mode.
+     * in kernel (not user) mode.ww
      */
     pid = waitReal(&status);
 
 } /* start2 */
+
+
+
+/* ------------------------------------------------------------------------
+   Name - spawn
+   Purpose - The system call handler for SYS_SPAWN
+   Parameters - A systemArgs pointer
+   Returns - n/a
+   Side Effects - n/a
+   ----------------------------------------------------------------------- */
+
+void spawn(systemArgs *args)
+{
+    /* parse the systemArgs argument and call spawnReal */
+    char *name, *argument;
+    int (*func)(char *);
+    int stacksize, priority, errors, spawnSuccess;
+
+    name = ((char *)args->arg5);
+    argument = ((char *)args->arg2);
+    func = (int(*)(char *))(args->arg1);
+    stacksize = *((int *)args->arg3);
+    priority = *((int *)args->arg4);
+    errors = 0;
+
+
+    /* check stacksize, name, func and priority */
+    if (stacksize < USLOSS_MIN_STACK) {
+        errors = 1;
+    }
+
+    if (name == NULL || func == NULL || priority < 1 || priority > 6) {
+        errors = 1;
+    }
+
+    if (errors) {
+        *(args->arg1) = (void *) -1;
+        *(args->arg4) = (void *) -1;
+        return;
+    }
+
+    
+    /* call spawn to create the user proces */
+    spawnSuccess = spawnReal(name, func, argument, stacksize, priority);
+
+    if (spawnSuccess == -1) {
+        *(args->arg1) = (void *) -1;
+        *(args->arg4) = (void *) -1;
+    }
+
+    *(args->arg4) = (void *) -1;
+    *(args->arg1) = (void *) spawnSuccess;
+
+    // terminate
+} /* spawn */
+
+
+
+/* ------------------------------------------------------------------------
+   Name - spawnReal
+   Purpose - n/a
+   Parameters - n/a
+   Returns - n/a
+   Side Effects - n/a
+   ----------------------------------------------------------------------- */
+
+int spawnReal(char *name, int (*func)(char *), char *arg, int stacksize, int priority)
+{
+    int pid;
+    procPtr slot, parent;
+
+
+    /* intialize values */
+    parent = &ProcTable[getpid() % MAXPROC];
+    pid = fork1(name, spawnLaunch, arg, stacksize, priority);
+    slot = &ProcTable[pid % MAXPROC];
+
+
+    /* add child process to parent */
+    parent->children = addChild(parent->children, slot);
+
+
+    /* fill out process table */
+    slot->pid = pid;
+    slot->func = func;
+    slot->children = NULL;
+
+
+    /* unblock spawnLaunch */
+    MboxSend(slot->mailbox, NULL, 0);
+
+
+    // put into process table
+    return pid;
+
+} /* spawnReal */
+
+
+
+/* ------------------------------------------------------------------------
+   Name - spawnLaunch
+   Purpose - n/a
+   Parameters - n/a
+   Returns - n/a
+   Side Effects - n/a
+   ----------------------------------------------------------------------- */
+
+int spawnLaunch(char *arg)
+{
+    int returnVal;
+    procPtr slot;
+
+
+    /* block spawnLaunch until process table filled in */
+    slot = &ProcTable[getpid() % MAXPROC];
+    MboxReceive(slot->mailbox, NULL, 0);
+
+    /* call function and return value if it is given call terminate() */
+    returnVal = slot->func(arg);
+
+    // call terminate
+    return returnVal;
+}
+
+
+
+/* ------------------------------------------------------------------------
+   Name - wait
+   Purpose - The system call handler for SYS_SPAWN
+   Parameters - A systemArgs pointer
+   Returns - n/a
+   Side Effects - n/a
+   ----------------------------------------------------------------------- */
+
+void wait(systemArgs *args) {
+
+}
+
+
+
+/* ------------------------------------------------------------------------
+   Name - wait
+   Purpose - The system call handler for SYS_SPAWN
+   Parameters - A systemArgs pointer
+   Returns - n/a
+   Side Effects - n/a
+   ----------------------------------------------------------------------- */
+
+void wait(systemArgs *args) {
+
+}
+
+
+
+/* ------------------------------------------------------------------------
+   Name - Terminate
+   Purpose - n/a
+   Parameters - n/a
+   Returns - n/a
+   Side Effects - n/a
+   ----------------------------------------------------------------------- */
+
+void terminate(systemArgs *args)
+{
+    int status;
+
+    status = *((int *)(args->args1));
+}
+
+
+
+/* ------------------------------------------------------------------------
+   Name - nullsys3
+   Purpose - Temporary function to call for the syscall handler
+   Parameters - n/a
+   Returns - n/a
+   Side Effects - Halts
+   ----------------------------------------------------------------------- */
+
+void nullsys3(systemArgs *args)
+{
+
+} /* nullsys3 */
+
+
+
+/* ------------------------------------------------------------------------
+   Name - addChild
+   Purpose - Temporary function to call for the syscall handler
+   Parameters - n/a
+   Returns - n/a
+   Side Effects - Halts
+   ----------------------------------------------------------------------- */
+
+procPtr addChild(procPtr head, procPtr toAdd)
+{
+
+} /* addChild */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- phase3.h
+++ phase3.h
@@ -7,5 +7,39 @@
 
 #define MAXSEMS         200
 
+
+/* function prototypes */
+extern void spawn(systemArgs *args);
+extern int spawnReal(char *name, int (*func)(char *), char *arg, int stacksize, 
+    int priority);
+extern int spawnLaunch(char *);
+
+
+/* typedef */
+typedef struct semaphore semaphore;
+typedef struct proc proc;
+typedef struct proc *procPtr;
+
+
+/* struct definitions */
+struct semaphore {
+    int count;
+};
+
+struct proc {
+    int         pid;
+    int         status;
+    int         mailBox;
+    int (*func)(char *);
+
+    /*  */
+    procPtr     children;
+    procPtr     sibling;
+};
+
+
+
+
+
 #endif /* _PHASE3_H */
 
